555f770af0e2feedd8b9c8a42906d07b2411e95b
Carlos D'Lacoste <cdlacostesalazar@gmail.com>
2024-08-29 14:43:06 -0400

    Add description to exercise 58

--- 58.length-of-last-word.js
+++ 58.length-of-last-word.js
@@ -2,6 +2,12 @@
  * @param {string} s
  * @return {number}
  */
+
+
+/*
+    Given a string s consisting of words and spaces, return the length of the last word in the string.
+    A word is a maximal substring consisting of non-space characters only.
+*/
 var lengthOfLastWord = function(s) {
     let wordsArray = s.split(' ').filter(palabra => palabra !== '');
     let lastWordArray = wordsArray[wordsArray.length - 1];
